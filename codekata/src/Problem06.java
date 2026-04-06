c60553431d874ee4c4d0906cf2de1dabc3a459ba
tjdgns0618 <gruddufs@gmail.com>
2026-04-06 09:05:56 +0900

    Problem06 두 수의 합 구하기 풀이, README 수정

---
+++ codekata/src/Problem06.java
@@ -0,0 +1,13 @@
+public class Problem06 {
+    public static void main(String[] args) {
+        Solution06 solution = new Solution06();
+        System.out.println(solution.solution(2,3));
+    }
+}
+
+class Solution06 {
+    public int solution(int num1, int num2) {
+        int answer = num1 + num2;
+        return answer;
+    }
+}
