95cc8886e09921c036fa83d607a512e203f099d7
tjdgns0618 <gruddufs@gmail.com>
2026-04-08 09:10:13 +0900

    Problem17 문자열을 정수로 바꾸기 풀이, README 수정

---
+++ codekata/src/Problem18.java
@@ -0,0 +1,13 @@
+public class Problem18 {
+    public static void main(String[] args) {
+        Solution18 s = new Solution18();
+        System.out.println(s.solution("-1234"));
+    }
+}
+
+class Solution18 {
+    public int solution(String s) {
+        int answer = Integer.parseInt(s);
+        return answer;
+    }
+}
